2da4936eca1129e8d56ead9248075660b336cb3e
Jobin2000 <126571891+Jobin2000@users.noreply.github.com>
2023-02-28 18:56:05 +0530

    Add files via upload

---
+++ testjo.py
@@ -0,0 +1,9 @@
+mark=70
+if mark >24:
+   print("you have passed the test")
+   if mark>90:
+       print("you have A+")
+   else:
+       print("you don't have A+")  
+else:
+    print("you have failed")
